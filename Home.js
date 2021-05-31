52d39df38f660d908c84e2cb61be8ac612da4e4b
Maphina <65483607+Maphina@users.noreply.github.com>
2021-05-31 17:55:20 +0545

    Add files via upload

---
+++ Home.js
@@ -0,0 +1,17 @@
+
+import BlogList from './BlogList';
+import useFetch from './useFetch';
+
+const Home = () => {
+    const{ data: blogs, isPending, error } = useFetch('https://jsonplaceholder.typicode.com/posts')
+     
+    return (  
+      <div className="home">
+      { error && <div>{ error }</div> }
+      { isPending && <div>Loading...</div> }
+      {blogs && <BlogList blogs={blogs} title="All Blogs"/>}
+    </div> 
+    );
+}
+ 
+export default Home;
